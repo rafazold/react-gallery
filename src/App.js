9b94b7a630adfc52a018ce2b90ecce151128c9f0
Rafa <zoldan.rafael@gmail.com>
2019-11-12 12:33:27 +0200

    added fetching info from API and rendering to page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,49 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            albums: [],
+            gallery: [],
+            albumId: ''
+        };
+    }
 
-  render() {
+    componentDidMount() {
+        fetch("https://jsonplaceholder.typicode.com/albums")
+            .then(res => res.json())
+            .then(albums => {
+                this.setState({albums});
+            });
+    }
+
+    addGallery(e) {
+        fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${e.target.value}`)
+            .then(res => res.json())
+            .then(gallery => {
+               this.setState({gallery});
+            });
+    }
+
+
+
+    render() {
     return (
         <div className="App">
-          <h1>Select an album:</h1>
-
+            <select defaultValue="" onChange={this.addGallery.bind(this)}>
+                  <option value="" disabled >Select your option</option>
+                      {this.state.albums.map((albums) => {
+                          return <option label={albums.title} value={albums.id} key={albums.id}>
+                              {albums.title}
+                          </option>
+                      })}
+            </select>
+            <div className="gallery">
+                {this.state.gallery.map(item => {
+                    return <img src={item.thumbnailUrl} alt="item" key={item.id}/>
+                })}
+            </div>
         </div>
     );
   }
